9f8585efc038d1e3137d288650b0a41a88b1f329
Bolley <yeahbolley@gmial.com>
2024-04-12 15:15:08 -0400

    3d model background farm

--- src/pages/GamePage/GamePage.jsx
+++ src/pages/GamePage/GamePage.jsx
@@ -1,33 +1,36 @@
-import React, { useState } from 'react'
-import GameContainer from '../../components/GameContainer/GameContainer'
-import StoreContainer from '../../components/StoreContainer/StoreContainer'
-import './GamePage.css'
+import React, { useState } from 'react';
+import GameContainer from '../../components/GameContainer/GameContainer';
+import StoreContainer from '../../components/StoreContainer/StoreContainer';
+import SplineBackground from './SplineBackground'; // Import the SplineBackground component
+import './GamePage.css';
 
 function GamePage() {
   const [cornVal, setCornVal] = useState(0);
   const [cornValMod_Passive, setCornValMod_Passive] = useState(0);
   const [cornValMod_Active, setCornValMod_Active] = useState(1);
+  
   return (
     <div className='GamePage'>
+      <SplineBackground /> {/* Include the SplineBackground component */}
       <h1>GamePage</h1>
       <div style={{ display: 'flex', justifyContent: 'center' }}>
         <div className='gameContainer'>
           <GameContainer 
-          cornVal={cornVal} 
-          setCornVal={setCornVal} 
-          cornValMod_Passive={cornValMod_Passive}
-          cornValMod_Active={cornValMod_Active} />
+            cornVal={cornVal} 
+            setCornVal={setCornVal} 
+            cornValMod_Passive={cornValMod_Passive}
+            cornValMod_Active={cornValMod_Active} />
         </div>
         <div className='storeContainer'>
           <StoreContainer 
-          cornVal={cornVal} 
-          setCornVal={setCornVal}
-          setCornValMod_Passive={setCornValMod_Passive}
-          setCornValMod_Active={setCornValMod_Active}  />
+            cornVal={cornVal} 
+            setCornVal={setCornVal}
+            setCornValMod_Passive={setCornValMod_Passive}
+            setCornValMod_Active={setCornValMod_Active}  />
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default GamePage
+export default GamePage;
